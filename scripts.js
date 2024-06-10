a3b8d7ace65d9fcb1caa22834631ce5794ca386c
Exet20 <risa-salek@seznam.cz>
2024-06-10 22:29:46 +0200

    files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const startDate = new Date('2022-02-21');
+const currentDate = new Date();
+
+const daysInMilliseconds = currentDate - startDate;
+
+// konvertovat na dny
+const daysInDevelopment = Math.floor(daysInMilliseconds / (1000 * 60 * 60 * 24));
+
+document.getElementById('daysInDevelopment').textContent = daysInDevelopment;
